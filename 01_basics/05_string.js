f438f2943f558cc092e8f87f92d4a266f781601b
Samriddhi4 <71831834+Samriddhi4@users.noreply.github.com>
2024-08-15 14:18:08 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,4 @@
+const name = "Samriddhi"
+const age = 24
+
+console.log(`My name is ${name} and my age is ${age}`)
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -3,3 +3,22 @@
 
 //Reference (Non-primitive)
 //array,objects,functions
+
+//++++++++++++++++++++++++++++
+// Stack memory(Primitive), Heap memory(Non-Primitive)
+let mychannelname = "WanderLust"
+
+let anothername = mychannelname
+anothername = "chai aur code"
+console.log(mychannelname);
+console.log(anothername);
+
+let userone = {
+    email: "samriddhixyz@gmail.com",
+    upi: "user@ybl"
+}
+//same reference for everything under heap
+let usertwo = userone
+usertwo.email = "user123@gmail.com"
+console.log(userone.email);
+console.log(usertwo.email);
