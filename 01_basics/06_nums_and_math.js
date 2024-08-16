96e5a4efe9faf59a86592abf8fa46296887f042d
Samriddhi4 <71831834+Samriddhi4@users.noreply.github.com>
2024-08-16 15:45:05 +0000

    date and number

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,9 @@
+const score = 400
+const balance = new Number(100)
+console.log(balance);
+console.log(typeof balance);
+
+//*******************Math*********************************//
+
+console.log(Math);
+console.log(Math.abs(-4));
---
+++ 01_basics/07_dates.js
@@ -0,0 +1,12 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toJSON());
+// console.log(typeof mydate)
+
+let myTimestamp = Date.now()
+console.log(myTimestamp)
+
+console.log(Date.now())
+console.log(Math.floor(Date.now()/1000))
