a387f66d62f5b944c423db931d582fe0b9c1de33
Samriddhi4 <71831834+Samriddhi4@users.noreply.github.com>
2024-08-18 09:34:22 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function saymyname(){
+    console.log("S"),
+    console.log("H"),
+    console.log("U"),
+    console.log("B"),
+    console.log("H"),
+    console.log("I")
+    
+}
+//saymyname()
+// function addtwonumbers(number1,number2){
+//     console.log(number1 + number2);
+// }
+
+function addtwonumbers(number1,number2){
+ let result = number1 + number2
+ return result
+}
+const result = addtwonumbers(3,4)
+// console.log("Result:",result);
+
+function loginusermessage(username){
+    return `${username} just logged in`
+}
+loginusermessage("samriddhi")
+console.log(loginusermessage("samriddhi"));
